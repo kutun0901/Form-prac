a6716a6564622be67577991ac6689ccd129ab546
John <tim@cruz-family.ws>
2023-02-02 15:21:19 -0800

    form and validations

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 
 const Form = () => {
@@ -9,27 +9,76 @@ const Form = () => {
     const [staff, setStaff] = useState("");
     const [bio, setBio] = useState("");
     const [subscribe, setSubscribe] = useState("");
+    const [validationErrors, setValidationErrors] = useState([]);
+    const [hasSubmitted, setHasSubmitted] = useState(false);
 
+    useEffect(() => {
+        const error = [];
+
+        if (!name) error.push('Please enter your name')
+        if (!email.includes('@')) error.push('Please provide a valid email')
+        if (!isNaN(phone)) error.push('Phone number should be properly formatted')
+        if (!phoneType) error.push('Please input a phone type')
+        if (bio.length > 280) error.push('Bio cannot reach 280 characters')
+
+        setValidationErrors(error)
+
+    }, [name, email, phone, phoneType, bio])
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        setHasSubmitted(true);
+        if (validationErrors.length) return alert('Cannot Submit')
+
+        const contactInfromation = {
+            name,
+            email,
+            phone,
+            phoneType,
+            bio,
+            submittedOn: new Date()
+        }
+
+        console.log(contactInfromation);
+        setName('');
+        setEmail('');
+        setPhone('');
+        setPhoneType('');
+        setBio('');
+        setValidationErrors([]);
+        setHasSubmitted(false);
+    }
 
     return (
         <div>
             <h1>User registration</h1>
-            <form>
+            {hasSubmitted && validationErrors.length > 0 && (
+                <div>
+                    The following errors were found:
+                    <ul>
+                        {validationErrors.map(error => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            <form onSubmit={onSubmit}>
                 <div>
                     <label>Name: </label>
-                    <input></input>
+                    <input onChange={e => setName(e.target.value)}></input>
                 </div>
                 <div>
-                    <label>Email: </label>
-                    <input></input>
+                    <label onChange={e => setEmail(e.target.value)}>Email: </label>
+                    <input ></input>
                 </div>
                 <div>
                     <label>Phone: </label>
-                    <input></input>
+                    <input onChange={e => setPhone(e.target.value)}></input>
                 </div>
                 <div>
                     <label>PhoneType: </label>
-                    <select>
+                    <select onChange={e => setPhoneType(e.target.value)}>
                         <option>Cell</option>
                         <option>Work</option>
                         <option>Home</option>
@@ -37,15 +86,15 @@ const Form = () => {
                 </div>
                 <div>
                     <label>Staff: </label>
-                    <input type="radio"></input>
+                    <input type="radio" onChange={e => setStaff(e.target.value)}></input>
                 </div>
                 <div>
                     <label>Bio: </label>
-                    <textarea></textarea>
+                    <textarea onChange={e => setBio(e.target.value)}></textarea>
                 </div>
                 <div>
                     <label>Sign Up for email notifications: </label>
-                    <input type="checkBox"></input>
+                    <input type="checkBox" onChange={e => setSubscribe(e.target.value)}></input>
                 </div>
             </form>
             <button>Submit</button>
